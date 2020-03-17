11e2715088d5c9b13946b0d7831d96650b5d74fc
Gaia <Gaia@LAPTOP-FBLQNBL2>
2020-03-17 12:29:11 +0100

    mia soluzione

--- Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/FXMLController.java
@@ -11,44 +11,89 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class FXMLController {
-	
-	Parole elenco ;
 
-    @FXML
-    private ResourceBundle resources;
+	Parole elenco;
 
-    @FXML
-    private URL location;
+	@FXML
+	private ResourceBundle resources;
 
-    @FXML
-    private TextField txtParola;
+	@FXML
+	private URL location;
 
-    @FXML
-    private Button btnInserisci;
+	@FXML
+	private TextField txtParola;
 
-    @FXML
-    private TextArea txtResult;
+	@FXML
+	private Button btnInserisci;
 
-    @FXML
-    private Button btnReset;
+	@FXML
+	private TextArea txtResult;
 
-    @FXML
-    void doInsert(ActionEvent event) {
-    	// TODO
-    }
+	@FXML
+	private TextArea txtTempiDiEsecuzione;
 
-    @FXML
-    void doReset(ActionEvent event) {
-    	// TODO
-    }
+	@FXML
+	private Button btnReset;
 
-    @FXML
-    void initialize() {
-        assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
-        assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+	@FXML
+	private Button btnCancella;
 
-        elenco = new Parole() ;
-    }
+	@FXML
+	void doInsert(ActionEvent event) {
+		String parola = txtParola.getText();
+		if (parola.length() == 0) {
+			txtResult.setText("Inserisci una parola!");
+			return;
+		}
+		boolean verbale = false;
+		try {
+
+			Integer.parseInt(parola);
+
+		} catch (Exception e) {
+			verbale = true;
+		}
+		
+		if(!verbale) {
+			txtResult.setText("Hai inserito un numero!");
+			return;
+		}
+		elenco.addParola(parola);
+		String risultato = "";
+		for (String s : elenco.getElenco())
+			risultato += s + "\n";
+		txtResult.setText(risultato);
+
+		txtTempiDiEsecuzione.appendText("" + System.nanoTime() + "\n");
+
+	}
+
+	@FXML
+	void doReset(ActionEvent event) {
+		elenco.reset();
+		txtResult.setText("");
+		txtTempiDiEsecuzione.appendText("" + System.nanoTime() + "\n");
+	}
+
+	@FXML
+	void doCancella(ActionEvent event) {
+		String parola = txtResult.getSelectedText();
+		elenco.cancellaParola(parola);
+		String risultato = "";
+		for (String s : elenco.getElenco())
+			risultato += s + "\n";
+		txtResult.setText(risultato);
+		txtTempiDiEsecuzione.appendText("" + System.nanoTime() + "\n");
+
+	}
+
+	@FXML
+	void initialize() {
+		assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
+		assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
+
+		elenco = new Parole();
+	}
 }
--- Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
+++ Lab01_Parole/src/main/java/it/polito/tdp/parole/model/Parole.java
@@ -1,24 +1,34 @@
 package it.polito.tdp.parole.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
+	//LinkedList<String>parole;
+	ArrayList<String>parole;
 		
 	public Parole() {
-		//TODO
+		parole=new ArrayList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		parole.add(p);
+		
 	}
 	
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		Collections.sort(parole, new ComparatoreParoleOrdineAlfabetico());
+		return parole;
 	}
 	
 	public void reset() {
-		// TODO
+		parole.clear();
+		}
+	
+	public void cancellaParola(String p) {
+		parole.remove(p);
 	}
 
 }
